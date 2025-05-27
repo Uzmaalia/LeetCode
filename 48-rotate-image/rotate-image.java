00342c8f80c89f62b7d1290375a83a718ed8a7b2
UzmaAalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-27 19:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (89.30%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        //transpose
+        int m = arr.length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        //90'rotate->reverse each row
+        for (int i = 0; i < m; i++) {
+            int a=0,b=m-1;
+            //swap arr[i][a] and arr[i][b]
+            while(a<b){
+                int temp=arr[i][a];
+                arr[i][a]=arr[i][b];
+                arr[i][b]=temp;
+                a++;
+                b--;
+            }
+        }
+    }
+}
