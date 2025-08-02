783a0db81cd18752b300bd579dcd790f35230270
UzmaAalia <118560215+Uzmaalia@users.noreply.github.com>
2025-08-02 14:05:09 +0530

    Time: 2 ms (79.94%) | Memory: 46.7 MB (5.93%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,38 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nonZeroIndex=0;
-        int n=nums.length;
+        //BRUTE
+        // int nonZeroIndex=0;
+        // int n=nums.length;
 
-        for(int i=0; i<n; i++){ //replacing zeros with nonzeros
-            if(nums[i] != 0 ){
-                nums[nonZeroIndex] = nums[i];
-                nonZeroIndex++;
+        // for(int i=0; i<n; i++){ //replacing zeros with nonzeros
+        //     if(nums[i] != 0 ){
+        //         nums[nonZeroIndex] = nums[i];
+        //         nonZeroIndex++;
+        //     }
+        // }
+        // for(int i=nonZeroIndex; i<n; i++){ //filling zeros at last
+        //     nums[i] = 0;
+        // }
+
+
+        //OPTIMAL
+        int n = nums.length;
+        int j =-1;
+        for(int i=0; i<n ; i++){
+            if(nums[i]==0){
+                j = i;
+                break;
             }
         }
-        for(int i=nonZeroIndex; i<n; i++){ //filling zeros at last
-            nums[i] = 0;
+        if (j == -1) return;
+        for(int i= j+1;i<n;i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
         }
-        
+        return;   
     }
 }
