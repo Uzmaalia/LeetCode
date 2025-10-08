a69e7e5b12caee50d75347427faa743e93dfb7e4
UzmaAalia <118560215+Uzmaalia@users.noreply.github.com>
2025-10-08 10:45:21 +0530

    Time: 1 ms (19.03%) | Memory: 46.1 MB (31.56%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,13 @@
 class Solution {
-    public int[] searchRange(int[] arr, int target) {
-        int[] ans = {-1,-1};
-        int n = arr.length;
-        //first check if ele is present
-        int lo= 0 ; int hi = n-1;
-        boolean flag = false;
-        while(lo<=hi){
-            int mid = lo + (hi - lo)/2;
-            if(arr[mid]==target){
-                flag=true;
-                break;
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int first=-1, last=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                if(first==-1) first = i;
+                last = i;
             }
-            else if (arr[mid]>target) hi = mid - 1;
-            else lo = mid + 1;
         }
-        if(flag == false) return ans;
-         
-        //lower bound 
-        lo = 0 ; hi = n-1;
-        int lb = n;
-        while(lo<=hi){
-            int mid = lo+(hi - lo )/2;
-            if(arr[mid]>=target){
-                lb = Math.min(lb , mid);
-                hi = mid -1;
-            }
-            else lo=mid+1;
-        }
-        ans[0] = lb;
-
-        //upper bound
-        lo = 0 ; hi=n-1;
-        int ub = arr.length;
-        while(lo<=hi){
-            int mid = lo+(hi - lo )/2;
-            if(arr[mid]>target){
-              ub = Math.min(ub , mid);
-              hi = mid - 1;
-            }
-            else lo = mid +1;
-        }
-        ans[1] = ub-1;
-        return ans;
+        return new int[]{first, last};
     }
 }
