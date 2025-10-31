417cbf47edb7b9ad158835a9427e8de4d820f874
UzmaAalia <118560215+Uzmaalia@users.noreply.github.com>
2025-10-31 17:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (7.41%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -8,16 +8,23 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//BRUTE
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slow =head ;
-        ListNode fast = head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        ListNode temp = head;
+        int cnt = 0;
+        while(temp != null){
+            cnt++;
+            temp = temp.next;
         }
-        return slow;
-
-    
+        int mid = (cnt/2)+1;
+        temp = head;
+        while(temp != null){
+            mid = mid -1;
+            if(mid == 0) break;
+            temp = temp.next;
+        }
+        return temp;
     }
 }
